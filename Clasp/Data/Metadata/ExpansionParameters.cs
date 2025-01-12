4e77d082d0dc5a2f5760c77f338cf7839c802176
Dendroid <duncanfmiller@gmail.com>
2025-01-12 06:23:36 -0500

    Aggregate expand-time parameters into a single object. Modify SourceLocation to include its source blob directly instead of the interface having to mandate it.

---
+++ Clasp/Data/Metadata/ExpansionParameters.cs
@@ -0,0 +1,24 @@
+﻿using Clasp.Binding;
+
+namespace Clasp.Data.Metadata
+{
+    internal sealed class ExpansionParameters
+    {
+        public readonly ExpansionEnv Env;
+        public readonly BindingStore Store;
+        public readonly int Phase;
+
+        public readonly ScopeTokenGenerator TokenGen;
+
+        public ExpansionParameters(ExpansionEnv env, BindingStore bs, int phase, ScopeTokenGenerator gen)
+        {
+            Env = env;
+            Store = bs;
+            Phase = phase;
+            TokenGen = gen;
+        }
+
+        public ExpansionParameters WithExtendedEnv() => new ExpansionParameters(new ExpansionEnv(Env), Store, Phase, TokenGen);
+        public ExpansionParameters WithNextPhase() => new ExpansionParameters(new ExpansionEnv(Env.TopLevel), Store, Phase + 1, TokenGen);
+    }
+}
--- Clasp/Data/Metadata/SourceLocation.cs
+++ Clasp/Data/Metadata/SourceLocation.cs
@@ -4,34 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Clasp.Data.Text;
+
 namespace Clasp.Data.Metadata
 {
     public struct SourceLocation
     {
         /// <summary>
-        /// The name of the location to which this <see cref="SourceLocation"/> refers. May be a file path or something else.
+        /// The name of the location to which this <see cref="SourceLocation"/> refers.
+        /// May be a file path or something else.
         /// </summary>
         public readonly string Source;
 
         /// <summary>
-        /// The line number (1-indexed) within the source text to which this <see cref="SourceLocation"/> refers.
+        /// The line number (1-indexed) within the source text to which this
+        /// <see cref="SourceLocation"/> refers.
         /// </summary>
         public readonly int LineNumber;
 
         /// <summary>
-        /// The column number (0-indexed by character) within the source text to which this <see cref="SourceLocation"/> refers.
+        /// The column number (0-indexed by character) within the source text to which this
+        /// <see cref="SourceLocation"/> refers.
         /// </summary>
         public readonly int Column;
 
         /// <summary>
-        /// The first character (1-indexed) of the source text (as an array of characters) to which this <see cref="SourceLocation"/> refers.
+        /// The first character (1-indexed) of the source text (as an array of characters) to which
+        /// this <see cref="SourceLocation"/> refers.
         /// </summary>
         public readonly int StartingPosition;
 
         /// <summary>
-        /// The length of the span of characters within the source text (as an array of characters) to which this <see cref="SourceLocation"/> refers.
+        /// The length of the span of characters within the source text (as an array of characters)
+        /// to which this <see cref="SourceLocation"/> refers.
         /// </summary>
         public readonly int Length;
+
+        /// <summary>
+        /// Indicates if this is the location of genuine code, or if it only indicates the location from
+        /// which generated code was derived.
+        /// </summary>
+        public readonly bool Original;
+
+        /// <summary>
+        /// The full text of <see cref="Source"/>, within which this location exists, if available.
+        /// </summary>
+        public readonly Blob SourceText;
 
         /// <summary>
         /// The <see cref="LineNumber"/> normalized to a 0-indexed system.
@@ -44,13 +62,20 @@ namespace Clasp.Data.Metadata
         public int NormalizedStartingPosition => StartingPosition - 1;
 
 
-        public SourceLocation(string source, int lineNumber, int column, int startingPosition, int length)
+        public SourceLocation(string source,
+            int lineNumber, int column, int startingPosition, int length,
+            Blob text, bool original = true)
         {
             Source = source;
             LineNumber = lineNumber;
             Column = column;
             StartingPosition = startingPosition;
             Length = length;
+
+            SourceText = text;
+            Original = original;
         }
+
+        public SourceLocation Derivation() => new SourceLocation(Source, LineNumber, Column, StartingPosition, 0, SourceText, false);
     }
 }
