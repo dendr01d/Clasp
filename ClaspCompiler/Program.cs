9d320871fc22059905823cfda814679e4c1b84ad
Duncan Miller <duncanfmiller@gmail.com>
2025-05-22 18:32:55 -0400

    Start building the compiler adjacently.

---
+++ ClaspCompiler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClaspCompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
