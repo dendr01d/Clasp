15fd3a0d4dd3d9318ac4d299b1eadfeefcdd26c3
Dendroid <duncanfmiller@gmail.com>
2025-07-10 23:18:51 -0400

    Add a bunch of special characters as constants.

---
+++ ClaspCompiler/Text/Unicode.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaspCompiler.Text
+{
+    internal static class Unicode
+    {
+        public const char Florin = 'ƒ';
+
+        public const char InvertedBang = '¡';
+        public const char BangBang = '‼';
+        public const char Interrobang = '‽';
+        public const char Ellipsis = '…';
+
+        public const char Pilcrow = '¶';
+
+        public const char Alpha = 'α';
+        public const char Beta = 'β';
+        public const char Gamma = 'γ';
+        public const char Delta = 'δ';
+        public const char Epsilon = 'ε';
+        public const char Zeta = 'ζ';
+        public const char Eta = 'η';
+        public const char Theta = 'θ';
+        public const char Iota = 'ι';
+        public const char Kappa = 'κ';
+        public const char Lambda = 'λ';
+        public const char Mu = 'μ';
+        public const char Nu = 'ν';
+        public const char Xi = 'ξ';
+        public const char Omicron = 'ο';
+        public const char Pi = 'π';
+        public const char Rho = 'ρ';
+        public const char Sigma = 'σ';
+        public const char Tau = 'τ';
+        public const char Upsilon = 'υ';
+        public const char Phi = 'φ';
+        public const char Chi = 'χ';
+        public const char Psi = 'ψ';
+        public const char Omega = 'ω';
+
+        public const char Sup0 = '⁰';
+        public const char Sup1 = '¹';
+        public const char Sup2 = '²';
+        public const char Sup3 = '³';
+        public const char Sup4 = '⁴';
+        public const char Sup5 = '⁵';
+        public const char Sup6 = '⁶';
+        public const char Sup7 = '⁷';
+        public const char Sup8 = '⁸';
+        public const char Sup9 = '⁹';
+
+        public const char Sub0 = '₀';
+        public const char Sub1 = '₁';
+        public const char Sub2 = '₂';
+        public const char Sub3 = '₃';
+        public const char Sub4 = '₄';
+        public const char Sub5 = '₅';
+        public const char Sub6 = '₆';
+        public const char Sub7 = '₇';
+        public const char Sub8 = '₈';
+        public const char Sub9 = '₉';
+
+        public const char Prime = '′';
+        public const char DoublePrime = '″';
+        public const char TriplePrime = '‴';
+
+        public const char Division = '÷';
+        public const char Multiplication = '×';
+
+        public const char UniversalQuantifier = 'Ɐ';
+        public const char ExistentialQuantifier = 'Ǝ';
+        public const char InexistentialQuantifier = '∄';
+
+        public const char Union = '⋃';
+        public const char Intersection = '⋂';
+
+        public const char LogicalAnd = '⋀';
+        public const char LogicalOr = '⋁';
+        public const char LogicalXor = '⊕';
+
+        public const char PartialDifferential = '∂';
+        public const char Gradient = '∆';
+        public const char Nabla = '∇';
+        public const char Sum = '∑';
+        public const char Product = '∏';
+        public const char Coproduct = '∐';
+
+        public const char Radical = '√';
+        public const char Infinity = '∞';
+        public const char Integral = '∫';
+
+
+        public const char DownTack = '⊤';
+        public const char UpTack = '⊥';
+        public const char Adjoint = '⊣';
+        public const char Turnstile = '⊢';
+
+        public const char TriRight = '⊳';
+        public const char TriLeft = '⊲';
+
+        public const char NotEqual = '≠';
+        public const char Equivalent = '≡';
+        public const char NotEquivalent = '≢';
+        public const char ApproximatelyEqual = '≈';
+
+        public const char Not = '¬';
+        public const char LessOrEqual = '≤';
+        public const char GreaterOrEqual = '≥';
+
+        public const char PlusOrMinus = '±';
+
+        public const char SuperSet = '⊃';
+        public const char SubSet = '⊂';
+        public const char EmptySet = '∅';
+
+        public const char Contains = '∈';
+        public const char ContainedBy = '∋';
+
+        public const char ArrowLeft = '←';
+        public const char ArrowUp = '↑';
+        public const char ArrowRight = '→';
+        public const char ArrowDown = '↓';
+        public const char ArrowBiHorizontal = '↔';
+        public const char ArrowBiVertical = '↕';
+
+        public const char ArrowBarLeft = '⇐';
+        public const char ArrowBarUp = '⇑';
+        public const char ArrowBarRight = '⇒';
+        public const char ArrowBarDown = '⇓';
+        public const char ArrowBiBarHorizontal = '⇔';
+        public const char ArrowBiBarVertical = '⇕';
+
+        public const char Bullet = '•';
+        public const char Starburst = '✶';
+        public const char Degree = '°';
+        public const char Composition = '∘';
+
+        public const char Venus = '♀';
+        public const char Mars = '♂';
+
+        public const char Spade = '♠';
+        public const char Club = '♣';
+        public const char Heart = '♥';
+        public const char Diamond = '♦';
+    }
+}
